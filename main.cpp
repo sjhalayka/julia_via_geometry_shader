35aaf614e201f137a8fa5ba6106aecb9206dcb09
Shawn Halayka <sjhalayka@gmail.com>
2021-08-08 19:29:13 -0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -23,6 +23,61 @@ using namespace std;
 #include "vertex_geometry_shader.h"
 
 
+class quaternion
+{
+public:
+	inline quaternion(void) : x(0.0f), y(0.0f), z(0.0f), w(0.0f) { /*default constructor*/ }
+	inline quaternion(const float src_x, const float src_y, const float src_z, const float src_w) : x(src_x), y(src_y), z(src_z), w(src_w) { /* custom constructor */ }
+
+	inline float self_dot(void) const
+	{
+		return x * x + y * y + z * z + w * w;
+	}
+
+	inline float magnitude(void) const
+	{
+		return sqrtf(self_dot());
+	}
+
+	quaternion operator*(const quaternion& right) const
+	{
+		quaternion ret;
+
+		ret.x = x * right.x - y * right.y - z * right.z - w * right.w;
+		ret.y = x * right.y + y * right.x + z * right.w - w * right.z;
+		ret.z = x * right.z - y * right.w + z * right.x + w * right.y;
+		ret.w = x * right.w + y * right.z - z * right.y + w * right.x;
+
+		return ret;
+	}
+
+	quaternion operator+(const quaternion& right) const
+	{
+		quaternion ret;
+
+		ret.x = x + right.x;
+		ret.y = y + right.y;
+		ret.z = z + right.z;
+		ret.w = w + right.w;
+
+		return ret;
+	}
+
+	quaternion operator-(const quaternion& right) const
+	{
+		quaternion ret;
+
+		ret.x = x - right.x;
+		ret.y = y - right.y;
+		ret.z = z - right.z;
+		ret.w = w - right.w;
+
+		return ret;
+	}
+
+
+	float x, y, z, w;
+};
 
 
 
@@ -74,15 +129,52 @@ int main(int argc, char **argv)
 	// Make enough data for 1 point
 	vector<float> point_vertex_data;
 
-	point_vertex_data.push_back(0);
-	point_vertex_data.push_back(0);
-	point_vertex_data.push_back(0);
-	point_vertex_data.push_back(0);
+	size_t res = 30;
 
-	point_vertex_data.push_back(0);
-	point_vertex_data.push_back(0);
-	point_vertex_data.push_back(0);
-	point_vertex_data.push_back(0);
+	float x_grid_max = 1.5;
+	float y_grid_max = 1.5;
+	float z_grid_max = 1.5;
+	float x_grid_min = -x_grid_max;
+	float y_grid_min = -y_grid_max;
+	float z_grid_min = -z_grid_max;
+	size_t x_res = res;
+	size_t y_res = res;
+	size_t z_res = res;
+
+	float z_w = 0;
+	quaternion C;
+	C.x = 0.3f;
+	C.y = 0.5f;
+	C.z = 0.4f;
+	C.w = 0.2f;
+	int max_iterations = 8;
+	float threshold = 4.0f;
+
+	const float x_step_size = (x_grid_max - x_grid_min) / (x_res - 1);
+	const float y_step_size = (y_grid_max - y_grid_min) / (y_res - 1);
+	const float z_step_size = (z_grid_max - z_grid_min) / (z_res - 1);
+
+	quaternion Z(x_grid_min, y_grid_min, z_grid_min, z_w);
+
+	for (size_t z = 0; z < z_res; z++, Z.z += z_step_size)
+	{
+		cout << "Z slice " << z + 1 << " of " << z_res << endl;
+
+		Z.x = x_grid_min;
+
+		for (size_t x = 0; x < x_res; x++, Z.x += x_step_size)
+		{
+			Z.y = y_grid_min;
+
+			for (size_t y = 0; y < y_res; y++, Z.y += y_step_size)
+			{
+				point_vertex_data.push_back(Z.x);
+				point_vertex_data.push_back(Z.y);
+				point_vertex_data.push_back(Z.z);
+				point_vertex_data.push_back(z_w);
+			}
+		}
+	}
 
 	const GLuint components_per_position = 4;
 	const GLuint components_per_vertex = components_per_position;
@@ -105,6 +197,10 @@ int main(int argc, char **argv)
 		0);
 
 	glUseProgram(g0_mc_shader.get_program());
+
+	glUniform4f(glGetUniformLocation(g0_mc_shader.get_program(), "C"), C.x, C.y, C.z, C.w);
+	glUniform1i(glGetUniformLocation(g0_mc_shader.get_program(), "max_iterations"), max_iterations);
+	glUniform1f(glGetUniformLocation(g0_mc_shader.get_program(), "threshold"), threshold);
 
 	size_t max_output_vertices_per_input = 502;
 
@@ -160,16 +256,25 @@ int main(int argc, char **argv)
 			feedback[feedback_index + 2] == 10000 &&
 			feedback[feedback_index + 3] == 10000)
 		{
-			cout << "found sentinel" << endl;
+			//cout << "found sentinel" << endl;
 			vec_sizes.push_back(curr_size);
 			curr_size = 0;
 		}
 		else
 		{
+			//cout << feedback[feedback_index + 0] << " " << feedback[feedback_index + 1] << "  " << feedback[feedback_index + 2] << " " << feedback[feedback_index + 3] << endl;
+
 			curr_size++;
 		}
 	}
 
 	cout << vec_sizes.size() << endl;
+
+
+
+
 	return 1;
 }
+
+
+
